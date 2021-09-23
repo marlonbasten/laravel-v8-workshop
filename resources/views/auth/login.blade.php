bcdcde50f8c08a1aa465dce4ce1d977bc64f6a49
Marlon Basten <mbasten@byte5.de>
2021-09-23 22:22:17 +0200

    feat: Changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,42 @@
-<x-guest-layout>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-6">
+
+                <div class="card">
+                    <div class="card-header">Anmelden</div>
+                    {{-- Start content --}}
+                    <div class="card-body">
+
+                        @include('components.errors')
+
+                        <form action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">E-Mail</label>
+                                <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" required autofocus>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Passwort</label>
+                                <input type="password" id="password" name="password" class="form-control" required autocomplete="current-password">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Anmelden">
+                            </div>
+                        </form>
+
+                    </div>
+                    {{-- End content --}}
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
+{{--<x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -53,4 +91,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout>--}}
