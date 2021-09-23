d794a1914bcdca18648af874d500db256bebb43c
Marlon Basten <mbasten@byte5.de>
2021-09-23 22:36:56 +0200

    feat: Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
 @auth
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-        <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+        <a class="navbar-brand" href="{{ route('index') }}">{{ config('app.name') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
